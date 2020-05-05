1a83a498a3fd983d5421e1f03a0df1db500f4801
SunWithIssues <anya.a.ellis@gmail.com>
2020-05-05 00:15:07 -0400

    Update GuessTest.java
    changes to test

--- P4Client/src/test/java/GuessTest.java
+++ P4Client/src/test/java/GuessTest.java
@@ -1,17 +1,51 @@
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Test;
+import java.io.Serializable;
+import java.util.function.Consumer;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-class GuessTest {
+import application.ClientThread;
+import application.GuessInfo;
 
+class GuessTest {
+	static GuessInfo newGame;
+	static ClientThread client;
+	
+	@BeforeEach
+	void setup() {
+		newGame = new GuessInfo();
+	}
+	
 	@Test
 	void test() {
-		fail("Not yet implemented");
+		assertEquals("GuessInfo", newGame.getClass().getName());
+		
+	}
+	
+	@Test
+	void test2() {
+		assertEquals(false, newGame.end);
+	}
+	
+	@Test
+	void test3() {
+		assertEquals(0, newGame.wordLength);
+	}
+	
+	@Test
+	void test4() {
+		assertEquals(0, newGame.correctWords);
+	}
+	
+	@Test
+	void test5() {
+		assertEquals(6, newGame.remainingGuess);
 	}
 
 }
