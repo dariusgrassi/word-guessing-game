c7d9e0510eeda50e4887fd245441f67c2ced9455
SunWithIssues <anya.a.ellis@gmail.com>
2020-05-05 00:15:47 -0400

    Update GuessInfo.java
    changes that fit test cases

--- P4Client/src/main/java/application/GuessInfo.java
+++ P4Client/src/main/java/application/GuessInfo.java
@@ -6,21 +6,23 @@ import java.util.ArrayList;
 public class GuessInfo implements Serializable {
 
     private static final long serialVersionUID = 1L;
-    int wordLength;
-    int remainingGuess;
-    int correctWords;
-    char guessedLetter;
+    public int wordLength;
+    public int remainingGuess;
+    public int correctWords;
+    public char guessedLetter;
+    int lineOfWord;
     String user;
 
-    boolean end;
+    public boolean end;
     boolean correct; // Assuming 
     ArrayList<Integer> catChosen = new ArrayList<>();
+    
 
-    GuessInfo(){
+    public GuessInfo(){
     	// Assuming that for remaingingGuess we count upto 6  // decrease it for the display - P
     	// Assuming that you start with 0 correct words
     	remainingGuess=6;
-        correctWords=0;
+        correctWords=wordLength=0;
         end = false;
     	
     }
